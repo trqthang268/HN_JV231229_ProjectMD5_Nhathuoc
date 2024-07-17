30af80b8adb639b0c7b1acb48f0304933e3c03bb
Trương Thắng <truongthang268@gmail.com>
2024-07-17 09:49:24 +0700

    first commit

---
+++ src/main/java/ra/hn_jv231229_projectmd5_nhathuoc/HnJv231229ProjectMd5NhathuocApplication.java
@@ -0,0 +1,13 @@
+package ra.hn_jv231229_projectmd5_nhathuoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnJv231229ProjectMd5NhathuocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HnJv231229ProjectMd5NhathuocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ra/hn_jv231229_projectmd5_nhathuoc/HnJv231229ProjectMd5NhathuocApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.hn_jv231229_projectmd5_nhathuoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HnJv231229ProjectMd5NhathuocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
